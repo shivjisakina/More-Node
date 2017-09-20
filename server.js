9881d0a40d61c88910a693d4c99bc32cd1207235
Sakina Shivji <shivjisakina@gmail.com>
2017-09-20 19:08:17 -0400

    verifying credit card numbers

--- server.js
+++ server.js
@@ -6,13 +6,19 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 var email = 'foo@bar.com';
-var boolean = "false"
+var boolean = "false";
+var creditcard = "4485618240638341"
+var fakecreditcard = "448561824063834"
 
 console.log(validator.isEmail(email)); //=> true
 
 console.log(validator.isBoolean(email)); //=> false
 
 console.log(validator.isBoolean(boolean)); //=> true
+
+console.log(validator.isCreditCard(creditcard)); //=> true
+
+console.log(validator.isCreditCard(fakecreditcard)); //=> false
 
 app.listen(port, function() {
     console.log("Listening on PORT " + port)})
