c8ebd6aae2f5c954b2609c07286061d10c3139fe
Sakina Shivji <shivjisakina@gmail.com>
2017-09-20 19:06:38 -0400

    validating booleans from strings

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 var email = 'foo@bar.com';
+var boolean = "false"
 
 console.log(validator.isEmail(email)); //=> true
+
+console.log(validator.isBoolean(email)); //=> false
+
+console.log(validator.isBoolean(boolean)); //=> true
 
 app.listen(port, function() {
     console.log("Listening on PORT " + port)})
