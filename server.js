c12ddc2cf66bcf673456256e39d49c809fd0c7fa
Sakina.Shivji <sakina.shivji@COMM-SHIVJI-MAC.local>
2017-09-21 09:55:30 -0400

    empty vs not empty string validation

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ var port = process.env.PORT || 3000;
 
 var email = 'foo@bar.com';
 var boolean = "false";
-var creditcard = "4485618240638341"
-var fakecreditcard = "448561824063834"
+var creditcard = "4485618240638341";
+var fakecreditcard = "448561824063834";
+var empty = "";
+var notEmpty = "not empty";
 
 console.log(validator.isEmail(email)); //=> true
 
@@ -19,6 +21,10 @@ console.log(validator.isBoolean(boolean)); //=> true
 console.log(validator.isCreditCard(creditcard)); //=> true
 
 console.log(validator.isCreditCard(fakecreditcard)); //=> false
+
+console.log(validator.isEmpty(empty)); //=> true
+
+console.log(validator.isEmpty(notEmpty)); //=> false
 
 app.listen(port, function() {
     console.log("Listening on PORT " + port)})
