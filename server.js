e1462a14986239283c085d6c499188e02dd47e75
Sakina.Shivji <shivjisakina@gmail.com>
2017-09-24 16:54:54 -0500

    validating an address

--- server.js
+++ server.js
@@ -48,5 +48,44 @@ console.log(validator.isFQDN(notDomain)); //=> false
 
 console.log(validator.isAlphanumeric(alphaNum)); //=> true
 
+
+//=======================================================
+
+var descriptor = {
+    name: {type: "string", required: true}
+};
+var asyncvalidator = new schema(descriptor);
+asyncvalidator.validate({name: "muji"}, function(errors, fields) {
+    if(errors) {
+        // validation failed, errors is an array of all errors
+        // fields is an object keyed by field name with an array of
+        // errors per field
+        console.log(handleErrors(errors, fields));
+    }
+    // validation passed
+    console.log("passed")
+});
+
+var descriptor = {
+    address: {
+        type: "object", required: true,
+        fields: {
+            street: {type: "string", required: true},
+            city: {type: "string", required: true},
+            zip: {type: "string", required: true, len: 8, message: "invalid zip"}
+        }
+    },
+    name: {type: "string", required: true}
+}
+var validator2 = new schema(descriptor);
+validator2.validate({ address: {} }, function(errors, fields) {
+    // errors for street, address.city, address.zip and address.name
+
+    //console.log(errors, fields)
+    for (var i = 0; i < errors.length; i++) {
+        console.log(errors[i].message)
+    }
+});
+
 app.listen(port, function() {
     console.log("Listening on PORT " + port)})
